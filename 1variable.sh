9a7750f47c2cfe1c3f5720e674c865bd044d2164
sowjanya mettu <sowjanyamettu.com>
2025-11-17 19:03:56 -0500

    varibales

---
+++ 1variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1=
+person2=
+echo "$person1 ::welcome to the America $person2"
+echo "$person2 :: Thankyou $person1"
+echo "$person2 Nice to meet you $person1"
+echo "$person1 :: Good to see you $person2"
+echo "$person1 :: Have a great day $person2"
+echo "$person2 :: You too $person1"
